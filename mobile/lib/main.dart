62d57773356a4d2d47dafbd60cadf3eac292270e
Rasik Dhakal <1dhakalrasik@gmail.com>
2026-07-02 17:24:24 +0545

    Dependencies injector (di) service loactor added

--- mobile/lib/main.dart
+++ mobile/lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/widgets.dart';  // importing flutter widgets package
-
 import 'app/app.dart';
+import 'core/di/service_locator.dart'; // service locator
 
-void main() {
+Future<void> main() async{ // void to future void cause we are awaiting setupDependencies()
+  WidgetsFlutterBinding.ensureInitialized(); //ensuring flutter initialized before running app.
+  await setupDependencies(); // setting up dependencies for service locator
   runApp(const App()); // starting app
 }
