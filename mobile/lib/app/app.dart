39cfe0853515be286bd364010708a1156ca1d8a1
Rasik Dhakal <1dhakalrasik@gmail.com>
2026-07-02 12:27:20 +0545

    feature first screen home page

--- mobile/lib/app/app.dart
+++ mobile/lib/app/app.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart'; // widgets packages
+import 'package:mobile/features/home/presentation/home_page.dart'; //home page 
+
+
 
 class App extends StatelessWidget {  // app class that extends stateless widget (ie widget that doesnt change or store any data and only displays UI)
   const App({super.key});  // constructor 
@@ -8,11 +11,7 @@ class App extends StatelessWidget {  // app class that extends stateless widget 
     return MaterialApp(
       title: 'Assistive Recognition System',
       debugShowCheckedModeBanner: false,  // the default debug banner on top right removed
-      home: const Scaffold( // first screen to show when app start
-        body: Center(
-          child: Text('Assistive Recognition System'),
-        ),
-      ),
+      home: const HomePage(), // first screen to show when app start,
     );
   }
 }
