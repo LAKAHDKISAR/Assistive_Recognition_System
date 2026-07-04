fc7c3475749d8f7d79ff52238ec869f5d67facfa
Rasik Dhakal <1dhakalrasik@gmail.com>
2026-07-04 17:50:47 +0545

    Camera page, servies and its navigation through app router

--- mobile/lib/app/router/app_router.dart
+++ mobile/lib/app/router/app_router.dart
@@ -1,10 +1,15 @@
 import "package:go_router/go_router.dart";
 import "package:mobile/features/home/presentation/home_page.dart";
+import "package:mobile/features/camera/presentation/pages/camera_page.dart";
 
 final GoRouter appRouter = GoRouter(
     routes: [
       GoRoute(
         path: "/",
         builder: (context, state) => const HomePage(), // route to home page when app start
+      ),
+      GoRoute(
+        path: "/camera",
+        builder: (context, state) => const CameraPage(),
       )]
 );
